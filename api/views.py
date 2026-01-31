27b6d6985d2c5e8e85a988741f52221f0c55fe1e
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2026-01-31 18:36:45 +0530

    Function based View Get Method

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 # Create your views here.
-def studentsView(req):
+
+@api_view(['GET'])
+def studentsView(request):
      #return Student data
     # students =[
     #     {'id':1,'name':'John Doe','class':'computer Science','age':25}
@@ -10,11 +16,18 @@ def studentsView(req):
     # return JsonResponse(students,safe=False)
 
     #return Student model
-    students = Student.objects.all()
+    # students = Student.objects.all()
     ## Manual serializations
-    students_list = list(students.values())
-    print(students)
-    return JsonResponse(students_list,safe=False)
+    # students_list = list(students.values())
+    # print(students)
+    # return JsonResponse(students_list,safe=False)
+
+    #function based view get Method
+    if request.method == 'GET':
+        #get all the data from Student Table
+        student = Student.objects.all()
+        serializer = StudentSerializer(student,many = True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
     
 
 
