d73f2ae003d54147ad533f29aa824de9471f9be9
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2026-02-04 13:46:28 +0530

    class Based Views

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,10 @@ urlpatterns=[
     path('students/',views.studentsView),
     path('students/<int:pk>/', views.studentDetailView),
 
+    path('employees/',views.Employees.as_view()),#for Class Based Views
+    path('employees/<int:pk>/',views.EmployeeDetail.as_view()),#for Accessing a  single Employee
+
+
 ]
 
 
