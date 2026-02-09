a1de5e8371f97ebbfe95ac5fae0bb667d2810d62
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2026-02-09 16:49:32 +0530

    Nested Serializer of Blog & Api

--- api/urls.py
+++ api/urls.py
@@ -12,7 +12,15 @@ urlpatterns=[
 
     # path('employees/',views.Employees.as_view()),#for Class Based Views
     # path('employees/<int:pk>/',views.EmployeeDetail.as_view()),#for Accessing a  single Employee
-    path('',include(router.urls))
+    path('',include(router.urls)),
+
+    #for blogs
+    path('blogs/',views.BlogsView.as_view()),
+    path('comments/',views.CommentsView.as_view()),
+
+    path('blogs/<int:pk>/',views.BlogDetailView.as_view()),
+    path('comments/<int:pk>/',views.CommentDetailView.as_view())
+
 
 
 ]
--- api/views.py
+++ api/views.py
@@ -9,6 +9,8 @@ from rest_framework.views import APIView
 from employees.models import Employee
 from django.http import Http404
 from rest_framework import mixins,generics,viewsets
+from blogs.models import Blog,Comment
+from blogs.serializers import BlogSerializer,CommentSerializer
 # Create your views here.
 
 @api_view(['GET','POST'])
@@ -189,7 +191,24 @@ class EmployeeViewset(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer     
 
-    
+class BlogsView(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+
+class CommentsView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    lookup_field = 'pk'
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+   queryset = Comment.objects.all()
+   serializer_class = CommentSerializer
+   lookup_field='pk'
+
     
 
 
